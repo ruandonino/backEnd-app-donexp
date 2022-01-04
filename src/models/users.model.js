e4746843fc52bcf0d6a90c63fba4cb6720073e62
DonRuan22 <ruan_donino@hotmail.com>
2022-01-04 14:59:49 -0300

    add function to add user ans shop

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -87,10 +87,34 @@ User.addUserAndShopById = async function (id,newUser, result) {
     //console.log(ret);
     //result(null, ret.rows[0]);
     if(length(ret.rows[0])==0){
-      shop_id = Shop.create({'name':newUser.name,'city':'','local_name':''});
-      newUser['id_loja'] = shop_id;
-      result_user = User.create(newUser);
-      result(null, ret.rows[0]);
+      try{
+        var ret_shop_id = await dbConn.execute("INSERT INTO SHOP (NAME,CITY,LOCAL_NAME) VALUES (:1,:2,:3) returning ID into :return_id",{1:newUser.name,2:'',3:'',return_id:{
+          dir: oracledb.BIND_OUT,
+          type: oracledb.NUMBER
+        }},{ autoCommit: true });
+      }
+      catch(err) {
+        console.log("error: ", err);
+        result(err, null);
+      }finally{
+        console.log(ret_shop_id);
+        var shop_id = ret_shop_id.outBinds.return_id[0];
+        newUser.vetor[3] = shop_id;
+        newUser.vetor['return_id'] = {
+          dir: oracledb.BIND_OUT,
+          type: oracledb.NUMBER
+        }
+        try{
+          var ret = await dbConn.execute("INSERT INTO USERS (NAME,EMAIL,ID_LOJA,POSITION) VALUES (:1,:2,:3,:4) returning ID into :return_id", newUser.vetor,{ autoCommit: true });
+        }catch(err){
+          console.log("error: ", err);
+          result(err, null);
+        }finally{
+          var user_id = ret.outBinds.return_id[0];
+          var retfinal = {'shop_id':shop_id,'user_id':user_id};
+          result(null, retfinal);
+        }
+      }
     }
     else{
       result(null, ret.rows[0]);
