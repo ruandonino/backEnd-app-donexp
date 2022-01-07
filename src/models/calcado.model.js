023fd708f096eba0a33e03a2ea7e7f70a3811453
DonRuan22 <ruan_donino@hotmail.com>
2022-01-07 14:52:11 -0300

    new create calcado

--- src/models/calcado.model.js
+++ src/models/calcado.model.js
@@ -54,6 +54,7 @@ Calcado.create = async function (newCalcado,result) {
     }finally{
       if(ret_verify_prod.rows !== undefined){
         console.log("VERIFY1");
+        console.log(ret_verify_prod.rows);
         data_verify=ret_verify_prod.rows[0][0];
         console.log("VERIFY");
         console.log(data_verify);
