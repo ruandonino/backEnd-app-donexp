54c53943e358daef9ac875e39ab22bee537701f3
DonRuan22 <ruan_donino@hotmail.com>
2022-01-10 16:57:27 -0300

    new findByProduct

--- src/routes/calcado.routes.js
+++ src/routes/calcado.routes.js
@@ -12,7 +12,7 @@ router.get('/shop/:id', calcadoController.findByIdShop);
 // Update a employee with id
 router.put('/:id', calcadoController.update);
 // Update a employee with id
-router.put('/product', calcadoController.findbyProduct);
+router.post('/product', calcadoController.findbyProduct);
 // Delete a employee with id
 router.delete('/:id', calcadoController.delete);
 module.exports = router
