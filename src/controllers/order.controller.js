c8894ae71ae90b7603b466e2c9273912be4c1746
DonRuan22 <ruan_donino@hotmail.com>
2022-01-17 17:35:02 -0300

    new list order by shop ID

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -55,6 +55,14 @@ exports.findByIdShop = function(req, res) {
   });
 };
 
+exports.listOrderByIdShop = function(req, res) {
+  Order.listOrderByIdShop(req.params.id, function(err, order) {
+  if (err)
+  res.send(err);
+  res.json(order);
+  });
+};
+
 exports.update = function(req, res) {
   if(req.body.constructor === Object && Object.keys(req.body).length === 0){
     res.status(400).send({ error:true, message: 'Please provide all required field' });
