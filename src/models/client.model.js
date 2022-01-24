23c6b90ee5bcd885bf6dba4e78780b73ce8fac94
DonRuan22 <ruan_donino@hotmail.com>
2022-01-24 16:40:07 -0300

    add quant media infoById

--- src/models/client.model.js
+++ src/models/client.model.js
@@ -142,7 +142,6 @@ Client.infoById = async function (id, result) {
     vector_info.push(nota);
     vector_info.push(media_products);
     vector_info.push(last_order);
-    vector_info.push(nota);
     vector_info.push(whats_app);
 
     result(null, vector_info);
