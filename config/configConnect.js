aa068137a742c1c7a4483bff92bdb5af76409110
DonRuan22 <ruan_donino@hotmail.com>
2022-01-25 17:42:20 -0300

    test global connection

--- config/configConnect.js
+++ config/configConnect.js
@@ -1,5 +1,5 @@
 const oracledb = require('oracledb');
-oracledb.initOracleClient({ libDir: 'C:\\Users\\Donruan\\Documents\\Projeto Smarex\\back-end-AI\\instantclient-basic-windows.x64-21.3.0.0.0\\instantclient_21_3' });
+//oracledb.initOracleClient({ libDir: 'C:\\Users\\Donruan\\Documents\\Projeto Smarex\\back-end-AI\\instantclient-basic-windows.x64-21.3.0.0.0\\instantclient_21_3' });
 // hr schema password
 var password = "231295Don**banco"
 // checkConnection asycn function
@@ -20,6 +20,4 @@ async function checkConnection() {
   }
 }
 
-connec = checkConnection();
-
-module.exports = connec;
+module.exports = checkConnection;
